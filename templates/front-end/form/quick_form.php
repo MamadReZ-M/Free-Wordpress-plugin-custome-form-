a278f23795065fc1de163f516cb84cd5d74d03b0
mohammadreza <137199553+MamadReZ-M@users.noreply.github.com>
2024-07-16 00:36:13 -0700

    Update quick_form.php

--- templates/front-end/form/quick_form.php
+++ templates/front-end/form/quick_form.php
@@ -1,38 +1,74 @@
-<form id="quick-form" action="" method="post" enctype="multipart/form-data">
-    <!-- Alert Message -->
-    <div id="alert-msg"></div>
-    <div class="row align-items-center">
-        <div class="col-md-12 mt-2 mb-2">
-            <div class="form-group">
-                <label for="full-name">نام و نام خانوادگی<b>*</b> </label>
-                <input type="text" id="full-name" name="full-name" placeholder="نام و نام خانوادگی">
-            </div>
-        </div>
-        <div class="col-md-12 mt-2 mb-2">
-            <div class="form-group">
-                <label for="phone-number">تلفن همراه<b>*</b> </label>
-                <input type="text" id="phone-number" name="phone-number" placeholder="شماره تماس">
-            </div>
-        </div>
-        <div class="col-md-12 mt-2 mb-2">
-            <p>شما چی ترجیح میدید؟</p>
-            <div class="form-check">
-                <input type="radio" name="prefer-call" id="phone" checked value="phone">
-                <label class="form-check-label" for="phone">
-                    تلفنی باهاتون صحبت کنیم
-                </label>
-            </div>
-            <div class="form-check">
-                <input type="radio" name="prefer-call" id="chat" value="chat">
-                <label class="form-check-label" for="chat">
-                    توی تلگرام باهاتون چت کنیم
-                </label>
-            </div>
-        </div>
-        <div class="col-md-12 mt-2 mb-2">
-            <div class="form-group">
-                <input type="submit" name="quick_confirm" id="quick_confirm" class="btn" value="دریافت مشاوره">
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+     aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body p-0">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div class="consultation-top">
+                    <div class="consultation-logo">
+                        <img src="<?= site_url() ?>/wp-content/uploads/hoovision_logo-512.webp"
+                             alt="تیم طراحی سایت هوویژن">
+                    </div>
+                </div>
+                <div class="consultation-content">
+                    <h2 class="text-center">تیم طراحی سایت هوویژن</h2>
+                    <p class="text-center">برای تماس، فرم را تکمیل کنید.</p>
+
+                    <form id="quick-form" action="" method="post" enctype="multipart/form-data">
+                        <!-- Alert Message -->
+                        <div id="alert-msg"></div>
+                        <div class="row align-items-center">
+                            <div class="col-md-12 mt-2 mb-2">
+                                <div class="form-group">
+                                    <label for="full-name">نام و نام خانوادگی<b>*</b> </label>
+                                    <input type="text" id="full-name" name="full-name" placeholder="نام و نام خانوادگی">
+                                </div>
+                            </div>
+                            <div class="col-md-12 mt-2 mb-2">
+                                <div class="form-group">
+                                    <label for="phone-number">تلفن همراه<b>*</b> </label>
+                                    <input type="text" id="phone-number" name="phone-number" placeholder="شماره تماس">
+                                </div>
+                            </div>
+                            <div class="col-md-12 mt-2 mb-2">
+                                <p>شما چی ترجیح میدید؟</p>
+                                <div class="form-check">
+                                    <input type="radio" name="prefer-call" id="phone" checked value="phone">
+                                    <label class="form-check-label" for="phone">
+                                        تلفنی باهاتون صحبت کنیم
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input type="radio" name="prefer-call" id="chat" value="chat">
+                                    <label class="form-check-label" for="chat">
+                                        توی تلگرام باهاتون چت کنیم
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="col-md-12 mt-2 mb-2">
+                                <div class="form-group">
+                                    <input type="submit" name="quick_confirm" id="quick_confirm" class="btn" value="دریافت مشاوره">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    <div class="consultation-phone">
+                        <span>ارتباط آنلاین با پشتیبانی:</span>
+                        <a href="https://t.me/hoovision">۰۹۹۴۱۳۹۷۳۶۷</a>
+                    </div>
+                    <hr>
+                    <div class="free-phone-consultation-plugin">
+                        <h4>
+                            <span>برای خرید</span>
+                            <a href="https://hoovision.com/plugins/free-phone-consultation-plugin/"
+                               aria-label="درباره افزونه ورپرس مشاوره رایگان">افزونه مشاوره رایگان</a>
+                            <span>کلیک کنید.</span>
+                        </h4>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</form>
+</div>
+
